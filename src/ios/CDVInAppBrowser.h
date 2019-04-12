0e12f81a98762af6483f2816455785859d5ee904
Sebastian Meyer <smeyer@lemon-blue.de>
2019-04-12 13:49:27 +0200

    fixed toolbar toggling

--- src/ios/CDVInAppBrowser.h
+++ src/ios/CDVInAppBrowser.h
@@ -70,7 +70,7 @@
 @property (nonatomic, assign) BOOL suppressesincrementalrendering;
 @property (nonatomic, assign) BOOL hidden;
 @property (nonatomic, assign) BOOL disallowoverscroll;
-@property (nonatomic, assign) BOOL enableToolbarToggle;
+@property (nonatomic, assign) BOOL enabletoolbartoggle;
 @property (nonatomic, assign) BOOL hidetoolbarafterstart;
 @property (nonatomic, assign) BOOL toolbaroverlay;
 
